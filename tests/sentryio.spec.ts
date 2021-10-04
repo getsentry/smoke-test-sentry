44e0c69092a4db2e9e63704787c61076bec24e66
Billy Vong <billy@sentry.io>
2021-10-04 19:58:00 -0400

    Initial commit

---
+++ tests/sentryio.spec.ts
@@ -0,0 +1,68 @@
+import {test, expect} from '@playwright/test';
+
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
+test.beforeAll(() => {
+  if (!process.env.SENTRY_DSN) {
+    return;
+  }
+
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 1.0,
+  });
+});
+
+test('sentryio', async ({page}) => {
+  const transaction = Sentry.startTransaction({
+    name: "sentry.io smoke test",
+  });
+
+  Sentry.getCurrentHub().configureScope(scope => scope.setSpan(transaction));
+
+  const span = transaction.startChild({
+    op: "smoke-test",
+    description: "run sentry.io smoke test",
+  });
+
+  try {
+    // Go to https://sentry.io/
+    await page.goto('https://sentry.io/');
+    expect(page.url()).toBe('https://sentry.io/welcome/');
+
+    // Click text=I Accept
+    await page.click('text=I Accept');
+
+    // Click a:has-text("Sign In")
+    await page.click('a:has-text("Sign In")');
+    await page.waitForURL('https://sentry.io/auth/login/');
+
+    // Click text=Single Sign-On
+    await page.click('a[href="#sso"]');
+
+    // Click [placeholder="acme"]
+    await page.click('[placeholder="acme"]');
+
+    // Fill [placeholder="acme"]
+    await page.fill('[placeholder="acme"]', 'sentry');
+
+    // Click #sso >> text=Continue
+    await page.click('#sso >> text=Continue');
+    expect(page.url()).toBe('https://sentry.io/auth/login/sentry/');
+
+    // Click text=Login with Google
+    await page.click('text=Login with Google');
+    await page.waitForURL('https://accounts.google.com/**/*');
+
+    // Close page
+    await page.close();
+    span.setStatus(Tracing.SpanStatus.Ok);
+  } catch (err) {
+    span.setStatus(Tracing.SpanStatus.InternalError);
+    throw err;
+  } finally {
+    span.finish();
+    transaction.finish();
+  }
+});
